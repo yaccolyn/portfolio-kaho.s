3c599a67d1625557f3f65da1ba6c65ef12ae57bf
yaccolyn <pari.pari.itigo@gmail.com>
2024-07-25 20:29:42 +0900

    edit機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = current_user.posts.find(params[:id])
+
+    unless current_user && current_user.own?(@post)
+      flash[:danger] = '編集できません'
+      redirect_to posts_path
+    end
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), success: '編集できました'
+    else
+      flash.now[:danger] = '編集できませんでした'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
